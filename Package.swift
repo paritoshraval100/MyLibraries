6d581a9091460d36ca674545fcc47d265ca95a7b
Paritosh Raval <paritosh.raval@infostretch.com>
2021-08-17 15:38:36 +0530

    Removed some files

--- Package.swift
+++ Package.swift
@@ -8,9 +8,6 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MyLibraries",
-            targets: ["MyLibraries"]),
-        .library(
             name: "Package1",
             targets: ["Package1"]),
         .library(
@@ -26,17 +23,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "MyLibraries",
-            dependencies: []),
-        .target(
             name: "Package1",
             dependencies: []),
         .target(
             name: "Package2",
             dependencies: []),
-        .testTarget(
-            name: "MyLibrariesTests",
-            dependencies: ["MyLibraries"]),
     ]
 )
 
--- Sources/MyLibraries/MyLibraries.swift
+++
@@ -1,7 +0,0 @@
-public struct MyLibraries {
-    var text = "Hello, World!"
-    
-    public static func myPackageName() -> String {
-        return "My Libraries"
-    }
-}
