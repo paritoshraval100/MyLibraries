6ed026711087f52f9754018bb9425aaba9ae6183
Paritosh Raval <paritosh.raval@infostretch.com>
2021-08-20 23:24:37 +0530

    Added files

--- Package.swift
+++ Package.swift
@@ -16,14 +16,15 @@ let package = Package(
 
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.0"))    ],
+        .package(url: "https://github.com/JohnSundell/Files.git", from: "4.2.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Package1",
             dependencies: [
-                .product(name: "Alamofire", package: "Alamofire"),
+
             ]),
         .target(
             name: "Package2",
