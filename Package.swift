4838037243c2f8b019625227ab2f9c455eee5acc
Paritosh Raval <paritosh.raval@infostretch.com>
2021-08-20 14:22:46 +0530

    Added dependancy

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
 
     ],
     dependencies: [
+        .package(url: "https://github.com/SwiftZip/SwiftZip.git", from: "0.0.5"),
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
