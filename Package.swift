a097ad6a87961336c4b4177d467e6d4a63af8b7b
Paritosh Raval <paritosh.raval@infostretch.com>
2021-08-20 23:39:52 +0530

    Added package3

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
 
     ],
     dependencies: [
-        .package(url: "https://github.com/JohnSundell/Files.git", from: "4.2.0"),
+        .package(url: "https://github.com/paritoshraval100/Package3.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,7 +24,7 @@ let package = Package(
         .target(
             name: "Package1",
             dependencies: [
-                "Files"
+                "Package3"
             ]),
         .target(
             name: "Package2",
